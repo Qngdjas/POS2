37592c246002c9d2ab84d9d58c593aea59993ac7
Qngdjas <qngdjas@mail.ru>
2022-04-03 23:31:17 +0800

    Initial commit

---
+++ src/com/company/State.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class State {
+
+    abstract void switchState();
+}
---
+++ src/com/company/TaskTracker.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class TaskTracker {
+    Process[] processes = new Process[3];
+
+}
